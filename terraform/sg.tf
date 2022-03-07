6ed09979c4098b1631a4c6e7a3de0e72dc85f7a9
Bash-mocart <bashirsuleiman77@gmail.com>
2022-03-07 08:42:50 +0100

    allowed access to worker node on port 1337

--- terraform/sg.tf
+++ terraform/sg.tf
@@ -6,7 +6,7 @@ resource "aws_security_group_rule" "cluster_inbound_22" {
   from_port                = 1337
   protocol                 =  "tcp"
   security_group_id        = aws_eks_cluster.eks.vpc_config[0].cluster_security_group_id
-  cidr_blocks              = [aws_vpc.main.cidr_block]
+  cidr_blocks              = ["0.0.0.0/0"]
 #   source_security_group_id = aws_security_group.eks_nodes.id
   to_port                  = 1337
   type                     = "ingress"
@@ -17,7 +17,7 @@ resource "aws_security_group_rule" "cluster_inbound_443" {
   from_port                = 443
   protocol                 =  "tcp"
   security_group_id        = aws_eks_cluster.eks.vpc_config[0].cluster_security_group_id
-  cidr_blocks              = [aws_vpc.main.cidr_block]
+  cidr_blocks              = ["0.0.0.0/0"]
 #   source_security_group_id = aws_security_group.eks_nodes.id
   to_port                  = 443
   type                     = "ingress"
